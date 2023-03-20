330a5313133bd9e571e9f2d86449b1ae5ed2a082
2AM-official <zhaoxinglyu@icloud.com>
2023-03-20 16:51:34 -0700

    achieve the basic functions

--- BrainCorp/PokemonAPI/pokemon.py
+++ BrainCorp/PokemonAPI/pokemon.py
@@ -3,46 +3,45 @@ import json
 import collections
 import heapq
 
-class pokemon:
-    def __init__(self) -> None:
+class pokemonCLI:
+    def __init__(self):
+        self.base_url = 'https://pokeapi.co/api/v2/pokemon/'
         self.move_counts = collections.defaultdict(int)
 
-    def get_top_moves(self, api):
-        for _ in range(5):
-            next_link, pokemons_apis = self.pokemons_get(api)
-            api = next_link
-            for pokemons_api in pokemons_apis:
-                self.moves_count(pokemons_api)
-        max_heap = []
-        for move, count in self.move_counts.items():
-            #print(move, count)
-            heapq.heappush(max_heap, (count, move))
-            if len(max_heap) > 10:
-                heapq.heappop(max_heap)
-        res = []
-        while max_heap:
-            count, move = heapq.heappop(max_heap)
-            res.append([count, move])
-        return res[::-1]
+    def lookup_by_name(self, name: str):
+        api = f"{self.base_url}{name.lower()}"
+        response = requests.get(api)
+        if response.status_code != 200:
+            print("Error: Pokémon not found.")
+            return []
+        data = response.json()
+        moves = [move['move']['name'] for move in data['moves']]
+        return sorted(moves)
+    
+    def lookup_by_number(self, number: int):
+        api = f"{self.base_url}{number}"
+        response = requests.get(api)
+        if response.status_code != 200:
+            print("Error: Pokémon not found.")
+            return []
+        data = response.json()
+        moves = [move['move']['name'] for move in data['moves']]
+        return sorted(moves)
 
-    def pokemons_get(self, api):
-        response_API = requests.get(api)
-        data = response_API.text
-        parse_json = json.loads(data)
-        next_link = parse_json['next']
-        pokemons_apis = []
-        for result in parse_json['results']:
-            pokemons_apis.append(result['url'])
-        return next_link, pokemons_apis
-
-    def moves_count(self, api):
-        response_API = requests.get(api)
-        data = response_API.text
-        parse_json = json.loads(data)
-        for move in parse_json['moves']:
-            move_name = move['move']['name']
-            self.move_counts[move_name] += 1
+    def get_top_moves(self) -> list:
+        """Return the top 10 moves from the first 5 pages of the Pokémon API."""
+        for i in range(1, 101):
+            moves = self.lookup_by_number(i)
+            for move in moves:
+                self.move_counts[move] += 1
+        min_heap = heapq.nsmallest(10, self.move_counts.items(), key=lambda x: -x[1])
+        return min_heap
 
 
-search = pokemon()
-print(search.get_top_moves('https://pokeapi.co/api/v2/pokemon'))
+
+
+search = pokemonCLI()
+print(search.get_top_moves())
+print(search.lookup_by_name('pikachu'))
+print(search.lookup_by_number(1))
+
